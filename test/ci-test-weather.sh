fa51f4680e3eaa0d2d14da129e6fd1ba474537c5
Federico M. Facca <federico.facca@gmail.com>
2017-04-11 14:43:39 +0200

    fixed ci shunit1 test for WeatherObserved

--- test/ci-test-weather.sh
+++ test/ci-test-weather.sh
@@ -5,10 +5,14 @@
 testWeatherObserved(){
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 33d93df... fixed ci shunit2 test for WeatherObserved
    result=`ajv compile --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
    assertEquals "schema Weather/WeatherObserved/schema.json is valid" "${result}"
    result=`ajv test --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherObserved/example.json --valid`
    assertEquals "Weather/WeatherObserved/example.json passed test" "${result}"
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> 62645bd... added example ci-test
@@ -20,6 +24,8 @@ testWeatherObserved(){
 >>>>>>> 62645bd... added example ci-test
 =======
 >>>>>>> 62645bd... added example ci-test
+=======
+>>>>>>> 33d93df... fixed ci shunit2 test for WeatherObserved
 }
 
 # load shunit2
