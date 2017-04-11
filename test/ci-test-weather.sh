3adedbff8f11a8b40650a72e69492678f82ea867
Federico M. Facca <federico.facca@gmail.com>
2017-04-11 14:27:19 +0200

    added example ci-test

--- test/ci-test-weather.sh
+++ test/ci-test-weather.sh
@@ -4,15 +4,21 @@
 
 testWeatherObserved(){
 <<<<<<< HEAD
+<<<<<<< HEAD
    result=`ajv compile --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
    assertEquals "schema Weather/WeatherObserved/schema.json is valid" "${result}"
    result=`ajv test --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherObserved/example.json --valid`
    assertEquals "Weather/WeatherObserved/example.json passed test" "${result}"
 =======
+=======
+>>>>>>> 62645bd... added example ci-test
    result = ajv compile --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json
    assertEquals "../Weather/WeatherObserved/example.json valid" "${result}"
    result = ajv test --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json -d ../Weather/WeatherObserved/example.json --valid
    assertEquals "schema ../Weather/WeatherObserved/schema.json is valid" "${result}"
+<<<<<<< HEAD
+>>>>>>> 62645bd... added example ci-test
+=======
 >>>>>>> 62645bd... added example ci-test
 }
 
