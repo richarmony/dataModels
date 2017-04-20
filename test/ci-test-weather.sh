c892ae5a7af81c88c07a82830111deaadf7c9f52
Federico M. Facca <federico.facca@gmail.com>
2017-04-20 21:40:46 +0200

    added ci for weather forecast model

--- test/ci-test-weather.sh
+++ test/ci-test-weather.sh
@@ -3,7 +3,6 @@
 # Tests the Weather datamodels
 
 testWeatherObserved(){
-<<<<<<< HEAD
    # 1. check that the JSON Schema is a valid JSON Schema
    result=`ajv compile --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
    assertEquals "schema Weather/WeatherObserved/schema.json is valid" "${result}"
@@ -11,12 +10,13 @@ testWeatherObserved(){
    # 2. check that the JSON example is valid according to the JSON Schema
    result=`ajv test --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherObserved/example.json --valid`
    assertEquals "Weather/WeatherObserved/example.json passed test" "${result}"
-=======
-   result = ajv compile --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json
-   assertEquals "../Weather/WeatherObserved/example.json valid" "${result}"
-   result = ajv test --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json -d ../Weather/WeatherObserved/example.json --valid
-   assertEquals "schema ../Weather/WeatherObserved/schema.json is valid" "${result}"
->>>>>>> 960f08a... added example ci-test
+}
+
+testWeatherForecast(){
+   result=`ajv compile --v5 -s Weather/WeatherForecast/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
+   assertEquals "schema Weather/WeatherForecast/schema.json is valid" "${result}"
+   result=`ajv test --v5 -s Weather/WeatherForecast/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherForecast/example.json --valid`
+   assertEquals "Weather/WeatherForecast/example.json passed test" "${result}"
 }
 
 # load shunit2
